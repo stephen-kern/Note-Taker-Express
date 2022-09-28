d135644de34e209c51841eeee01723b66bb5ada7
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-09-28 16:05:40 -0500

    deleted hard-coded object in the db, finished the api and html routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,1 +1,27 @@
+// include packages
 const router = require("express").Router();
+const fs = require("fs");
+
+// require the json data file
+const noteData = require("../../db/db.json");
+
+// import the functions that handle the notes
+const { createNote, validateNote, deleteNote } = require("../../lib/notes");
+
+router.get("/notes", (req, res) => {
+    let results = noteData;
+    res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+    let notes = createNote(req.body, noteData);
+    validateNote(notes);
+    res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    deleteNote(noteData, req.params.id);
+    res.json(noteData);
+});
+
+module.exports = router;
